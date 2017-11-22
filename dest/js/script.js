d3a499e29b2d753be10a0308c17d19ecfd01ba85
Samuel Gray <sam.gray22@gmail.com>
2017-11-22 12:52:03 +0000

    Opportunity

---
+++ dest/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.number').each(function() {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function(now) {
+        $(this).text(Math.ceil(now));
+        var text = $(this).text;
+        text = text + "%";
+      }
+    });
+  });
+
+});
