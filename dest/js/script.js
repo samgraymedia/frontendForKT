32d738801256acc03e92893f62a27576f9e1aa1d
Samuel Gray <sam.gray22@gmail.com>
2017-11-22 18:24:05 +0000

    commit

--- dest/js/script.js
+++ dest/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  //number counter
   $('.number').each(function() {
     $(this).prop('Counter', 0).animate({
       Counter: $(this).text()
@@ -12,5 +13,6 @@ $(document).ready(function() {
       }
     });
   });
+
 
 });
